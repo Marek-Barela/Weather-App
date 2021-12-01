05afcbd05b2026fec768ded6bff08a3d23517b2b
Marek Bareła <marek@alergeek.ventures>
2021-12-01 19:53:24 +0100

    feat: add 24 hour forecast for DetailsForecastView component

--- common/helpers/msToTime.ts
+++ common/helpers/msToTime.ts
@@ -2,7 +2,8 @@ export const msToTime = (duration: number) => {
   const minutes = Math.floor((duration / (1000 * 60)) % 60);
   const hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
 
+  const minutesAsString = minutes < 10 ? '0' + minutes : minutes;
   const hoursAsString = hours < 10 ? '0' + hours : hours;
 
-  return hoursAsString + ':' + minutes;
+  return hoursAsString + ':' + minutesAsString;
 };
