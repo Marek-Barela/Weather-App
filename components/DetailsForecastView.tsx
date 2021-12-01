cda27ca00d922120910522f8ddd28a54c56da003
Marek Bareła <marek@alergeek.ventures>
2021-12-01 18:43:49 +0100

    feat: add seven days forecast to DetailsForecastView component

--- components/DetailsForecastView.tsx
+++ components/DetailsForecastView.tsx
@@ -6,10 +6,12 @@ import {
   Input,
   InputGroup,
   InputRightElement,
+  Text,
 } from '@chakra-ui/react';
 import type { City } from 'common/cities';
 import { useFetchOneCallWeatherForecast } from 'common/hooks/useFetchOneCallWeatherForecast';
 import CurrentWeatherContainer from 'components/CurrentWeatherContainer';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { Search } from 'react-feather';
@@ -29,8 +31,8 @@ const DetailsForecastView = ({ city }: DetailsForecastViewProps) => {
   console.log(weatherForecast);
 
   return (
-    <Box minH='600px' height='100vh' bgColor='#82B7F1'>
-      <Flex minH='100%' bgColor='#F2FBFF'>
+    <Box minH='600px' height='100vh'>
+      <Flex minH='100%'>
         <Flex width='70%' padding='60px' direction='column'>
           <Flex justifyContent='space-between' alignItems='center'>
             <Heading as='h2' fontSize='3em' textTransform='capitalize'>
@@ -58,6 +60,35 @@ const DetailsForecastView = ({ city }: DetailsForecastViewProps) => {
                 </InputRightElement>
               </InputGroup>
             </form>
+          </Flex>
+          <Flex justifyContent='center' mt='30px'>
+            {weatherForecast?.data.daily.map((forecast, index) => (
+              <Flex
+                key={index}
+                direction='column'
+                padding='15px'
+                margin='10px'
+                borderRadius='5px'
+                bgColor='white'
+                width='300px'>
+                <Text fontSize='0.7em' fontWeight='bold' textAlign='center'>
+                  {new Date(forecast.dt * 1000).toLocaleString('en-us', {
+                    weekday: 'long',
+                  })}
+                </Text>
+                <Image
+                  src={`https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`}
+                  width='30px'
+                  height='50px'
+                />
+                <Flex fontSize='0.7em' justifyContent='center' fontWeight='bold'>
+                  <Text mr='6px'>{Math.round(forecast.temp.day)}&#8451;</Text> |
+                  <Text color='gray.400' ml='6px'>
+                    {Math.round(forecast.temp.night)}&#8451;
+                  </Text>
+                </Flex>
+              </Flex>
+            ))}
           </Flex>
         </Flex>
         <CurrentWeatherContainer
