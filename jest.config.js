8374108966fe86926e2fb192336c4f6dc4a85ab3
Marek Bareła <marek@alergeek.ventures>
2021-12-02 15:44:55 +0100

    test: add test case for CurrentWeatherContainer, install jest-date-mock to mock date and update jest config to resolve problems

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
+  roots: ['<rootDir>'],
+  collectCoverageFrom: ['{common,components,pages}/**/*.{ts,tsx,js,jsx}'],
+  setupFilesAfterEnv: [],
+  moduleNameMapper: {
+    '^components/(.*)$': '<rootDir>/components/$1',
+    '^pages/(.*)$': '<rootDir>/pages/$1',
+    '^common/(.*)$': '<rootDir>/common/$1',
+    '^tests/(.*)$': '<rootDir>/tests/$1',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
+  testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  testEnvironment: 'jsdom',
 };
