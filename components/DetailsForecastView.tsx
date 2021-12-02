883a7a1d96d342148e7cebe20a5c9f7ce68b2deb
Marek Bareła <marek@alergeek.ventures>
2021-12-02 05:14:36 +0100

    revactor: export 7 days widget into samller component

--- components/DetailsForecastView.tsx
+++ components/DetailsForecastView.tsx
@@ -7,13 +7,12 @@ import {
   Input,
   InputGroup,
   InputRightElement,
-  Text,
 } from '@chakra-ui/react';
 import type { City } from 'common/cities';
 import { useFetchOneCallWeatherForecast } from 'common/hooks/useFetchOneCallWeatherForecast';
 import CurrentWeatherContainer from 'components/CurrentWeatherContainer';
 import HoursForecastChart from 'components/HoursForecastChart';
-import Image from 'next/image';
+import WeekForecastWidget from 'components/WeekForecastWidget';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { Search } from 'react-feather';
@@ -64,40 +63,7 @@ const DetailsForecastView = ({ city }: DetailsForecastViewProps) => {
           <Divider />
           <HoursForecastChart weatherForecast={weatherForecast} />
           <Divider />
-          <Flex justifyContent='center' mt='20px' flexDirection='column'>
-            <Heading as='h3' textAlign='center' fontWeight='bold' fontSize='1.2em'>
-              7 Days Forecast
-            </Heading>
-            <Flex width='100%'>
-              {weatherForecast?.data.daily.map((forecast, index) => (
-                <Flex
-                  key={index}
-                  direction='column'
-                  padding='15px'
-                  margin='10px'
-                  borderRadius='5px'
-                  bgColor='white'
-                  alignItems='center'>
-                  <Text fontSize='0.7em' fontWeight='bold' textAlign='center'>
-                    {new Date(forecast.dt * 1000).toLocaleString('en-us', {
-                      weekday: 'long',
-                    })}
-                  </Text>
-                  <Image
-                    src={`https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`}
-                    width='50px'
-                    height='50px'
-                  />
-                  <Flex fontSize='0.7em' justifyContent='center' fontWeight='bold'>
-                    <Text mr='6px'>{Math.round(forecast.temp.day)}&#8451;</Text> |
-                    <Text color='gray.400' ml='6px'>
-                      {Math.round(forecast.temp.night)}&#8451;
-                    </Text>
-                  </Flex>
-                </Flex>
-              ))}
-            </Flex>
-          </Flex>
+          <WeekForecastWidget weatherForecast={weatherForecast} />
         </Flex>
         <CurrentWeatherContainer
           weatherForecast={weatherForecast}
